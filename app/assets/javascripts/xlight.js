6b81f1a8b1806c0753f656e347ce797d8f70f4c2
tmtuan <tmtuan.projects@gmail.com>
2015-11-03 10:18:51 +0700

    Initializes blogshare

---
+++ app/assets/javascripts/xlight.js
@@ -0,0 +1,8 @@
+//=require xlight/bootstrap.min
+//=require xlight/bootstrap-checkbox-radio-switch
+//=require xlight/bootstrap-notify
+//=require xlight/bootstrap-select
+//=require xlight/jquery-1.10.2
+//=require xlight/chartist.min
+//=require xlight/light-bootstrap-dashboard
+
